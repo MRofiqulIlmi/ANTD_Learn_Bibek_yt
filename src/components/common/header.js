b59f85026e865cb34ed42e3ec48023a0d3459f1e
Rofiq2 <muhammadrofiqulilmi@gmail.com>
2022-10-21 16:17:42 +0700

    menu with drawer component

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,8 +1,15 @@
-import React from "react";
-import { Anchor } from 'antd';
+import React, {useState} from "react";
+import { Anchor, Button, Drawer } from 'antd';
 const {Link} = Anchor;
 
 function AppHeader(){
+    const [open, setOpen] = useState(false);
+    const showDrawer = () => {
+        setOpen(true);
+    };
+    const onClose = () => {
+        setOpen(false);
+    };
     return(
         <div className="container-fluid">
             <div className="header">
@@ -10,16 +17,34 @@ function AppHeader(){
                     <i className="fa fa-bolt"></i>
                     <a href="#">Tech</a>
                 </div>
-                <Anchor targetOffset="65">
-                    <Link href="#hero" title="Home" />
-                    <Link href="#about" title="About" />
-                    <Link href="#feature" title="Features"/>
-                    <Link href="#works" title="How It Works" />
-                    <Link href="#faq" title="FAQ" />
-                    <Link href="#pricing" title="Pricing" />
-                    <Link href="#contact" title="Contact" />
-                    
-                </Anchor>
+                <div className="mobileHidden">
+                    <Anchor targetOffset="65">
+                        <Link href="#hero" title="Home" />
+                        <Link href="#about" title="About" />
+                        <Link href="#feature" title="Features"/>
+                        <Link href="#works" title="How It Works" />
+                        <Link href="#faq" title="FAQ" />
+                        <Link href="#pricing" title="Pricing" />
+                        <Link href="#contact" title="Contact" />
+                    </Anchor>
+                </div>
+                <div className="mobileVisible">
+                    <Button type="primary" onClick={showDrawer}>
+                        <i className="fas fa-bars"></i>
+                    </Button>
+                </div>
+                <Drawer title="Menu" placement="right" onClose={onClose} open={open}>
+                    <Anchor targetOffset="65">
+                        <Link href="#hero" title="Home" />
+                        <Link href="#about" title="About" />
+                        <Link href="#feature" title="Features"/>
+                        <Link href="#works" title="How It Works" />
+                        <Link href="#faq" title="FAQ" />
+                        <Link href="#pricing" title="Pricing" />
+                        <Link href="#contact" title="Contact" />
+                    </Anchor>
+                </Drawer>
+                
             </div>
         </div>
     )
